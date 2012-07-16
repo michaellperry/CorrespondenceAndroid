e287a029d741511a141f2dc27151fda5d904f511
Michael L Perry <mperry@adventuresinsoftware.com>
2012-07-15 21:26:05 -0500

    Update task name on list view.

--- examples/honeydolist/honeydolist/src/com/facetedworlds/honeydolist/TaskListActivity.java
+++ examples/honeydolist/honeydolist/src/com/facetedworlds/honeydolist/TaskListActivity.java
@@ -15,6 +15,9 @@ import android.widget.TextView;
 import com.facetedworlds.honeydolist.adapters.IdentityListShareComparator;
 import com.facetedworlds.honeydolist.adapters.TaskListAdapter;
 import com.facetedworlds.honeydolist.collaboration.SynchronizationService;
+import com.updatecontrols.Dependent;
+import com.updatecontrols.UpdateMethod;
+import com.updatecontrols.android.Update;
 
 import facetedworlds.honeydo.model.IdentityListShare;
 import facetedworlds.honeydo.model.List__name;
@@ -22,6 +25,9 @@ import facetedworlds.honeydo.model.List__name;
 public class TaskListActivity extends Activity {
 
 	private IdentityListShare share;
+	private TextView titleTextView;
+	
+	private Dependent depTitle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +43,13 @@ public class TaskListActivity extends Activity {
 			if (position >= 0 && position < sortedShares.size()) {
 				share = sortedShares.get(position);
 				
-				TextView title = (TextView)findViewById(R.id.list_name);
-				String name = "";
-				for (List__name candidate : share.getList().nameCandidates()) {
-					name = candidate.getValue();
-					break;
-				}
-				title.setText(name);
+				titleTextView = (TextView)findViewById(R.id.list_name);
+				depTitle = Update.whenNecessary(this, new UpdateMethod() {
+					@Override
+					public void update() {
+						titleTextView.setText(getListName());
+					}
+				});
 		        
 		        View taskListView = findViewById(R.id.taskList);
 				ListView taskList = (ListView)taskListView;
@@ -58,6 +64,15 @@ public class TaskListActivity extends Activity {
 		} catch (Throwable t) {
 			Log.e("TaskListActivity", t.getMessage(), t);
 		}
+	}
+
+	private String getListName() {
+		String name = "";
+		for (List__name candidate : share.getList().nameCandidates()) {
+			name = candidate.getValue();
+			break;
+		}
+		return name;
 	}
 
 	private void onSelectTask(int position) {
--- examples/honeydolist/honeydolist/src/com/facetedworlds/honeydolist/adapters/TaskListAdapter.java
+++ examples/honeydolist/honeydolist/src/com/facetedworlds/honeydolist/adapters/TaskListAdapter.java
@@ -7,8 +7,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.updatecontrols.Dependent;
-import com.updatecontrols.InvalidatedListener;
 import com.updatecontrols.UpdateMethod;
+import com.updatecontrols.android.Update;
 
 import facetedworlds.honeydo.model.List;
 import facetedworlds.honeydo.model.Task;
@@ -25,35 +25,20 @@ public class TaskListAdapter extends ArrayAdapter<TaskAdapter> {
 		
 		this.list = list;
 		this.context = context;
-		depListSummaries = new Dependent(new UpdateMethod() {
+		
+		depListSummaries = Update.whenNecessary(context, new UpdateMethod() {
 			@Override
 			public void update() {
 				updateListSummaries();
 			}
 		});
-		
-		depListSummaries.addInalidatedListener(new InvalidatedListener() {
-			@Override
-			public void invalidated() {
-				context.runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						depListSummaries.onGet();
-					}
-				});
-			}
-		});
-		depListSummaries.onGet();
 	}
 
 	private void updateListSummaries() {
-		Log.i("TaskListAdapter", "updateListSummaries");
-		
 		this.clear();
 		for (Task task : list.tasks()) {
 			this.add(new TaskAdapter(task, context));
 		}
-		Log.i("TaskListAdapter", "updateListSummaries - done");
 	}
 
 	@Override
